22cc9186ae31f79e98153f5b2270197c7b3b33ef
Dhanush2008 <75597875+Dhanush2008@users.noreply.github.com>
2021-08-17 12:34:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Opening console project");
+var Firstname= "Dhanush";
+var Lastname= "Boyineni";
+
+var Fullname= Firstname.concat(Lastname);
+console.log(Fullname);
+
+var maxnum=Math.max("25", "47", "39", "23");
+console.log(maxnum);
+
+var sqrt=Math.sqrt("64");
+var power=Math.pow(3,3);
+
+console.log(sqrt);
+console.log(power);
