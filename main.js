90c8895ea9e6d9f7a7af0b3c9e1c462ddbcd6682
Dhanush2008 <75597875+Dhanush2008@users.noreply.github.com>
2021-08-21 17:52:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log("Opening console project");
+var Firstname= "Dhanush";
+var Lastname= "Boyineni";
+
+var Fullname= Firstname.concat(Lastname);
+console.log(Fullname);
+
+var maxnum=Math.max("25", "47", "39", "23");
+console.log(maxnum);
+
+var sqrt=Math.sqrt("64");
+var power=Math.pow(3,3);
+
+console.log(sqrt);
+console.log(power);
+
+var num1=(5);
+var num2=(23);
+var num3=(7);
+var num4=(69);
+var add=num1 + num2;
+console.log(add);
+
+var subtract= num2 - num1;
+console.log(subtract);
+
+var multiply=num1 * num3;
+console.log(multiply);
+
+var divide= num4 / num2;
+console.log(divide);
